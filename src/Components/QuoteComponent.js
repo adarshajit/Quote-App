4ae4be9e3e7e85ce3bde494758b621224f814b66
adarsh_ajit <adarshajit@gmail.com>
2020-08-28 01:10:20 +0530

    fetched data from an API

--- src/Components/QuoteComponent.js
+++ src/Components/QuoteComponent.js
@@ -3,20 +3,32 @@ import React,{Component} from 'react';
 
 
 class Quote extends Component{
+
     constructor(){
         super()
         this.state={
-            data : []
+            quoteData : [],
+            authorData : []
+
         }
     }
 
-
+    componentDidMount(){
+        fetch('https://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+          this.setState({quoteData : data.content})
+          this.setState({authorData : data.author})
+        })
+    }
 
 
     render(){
         return(
             <div>
-                <h1>Quote part</h1>
+                <h1>Quote part</h1>  
+                <h2>{this.state.quoteData}</h2>  
+                <h3>-{this.state.authorData}</h3>   
             </div>
         )
     }
