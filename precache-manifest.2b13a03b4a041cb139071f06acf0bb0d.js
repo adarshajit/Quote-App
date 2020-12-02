03d53f7c1d6827bd2b76c3afba8ea2366022387d
adarshajit <adarshajit@gmail.com>
2020-12-02 14:24:34 +0530

    Updates

---
+++ precache-manifest.2b13a03b4a041cb139071f06acf0bb0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0abf6a6f57b247661435300e7850ccc",
+    "url": "/Quote-App/index.html"
+  },
+  {
+    "revision": "4acc744a7e8325e27fec",
+    "url": "/Quote-App/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fb808b7236b16ea8af1a",
+    "url": "/Quote-App/static/css/main.fe1d9dbe.chunk.css"
+  },
+  {
+    "revision": "4acc744a7e8325e27fec",
+    "url": "/Quote-App/static/js/2.587740de.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/Quote-App/static/js/2.587740de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb808b7236b16ea8af1a",
+    "url": "/Quote-App/static/js/main.6d0a1c1c.chunk.js"
+  },
+  {
+    "revision": "5d243066ebb5fdacd475",
+    "url": "/Quote-App/static/js/runtime-main.a53a5820.js"
+  },
+  {
+    "revision": "3068f3c2d65185f4964bdc8c604ce007",
+    "url": "/Quote-App/static/media/bg.3068f3c2.jpg"
+  },
+  {
+    "revision": "865773a434da0c2f54fde7733e1ca32a",
+    "url": "/Quote-App/static/media/bg2.865773a4.jpg"
+  },
+  {
+    "revision": "cb3affd3f911758ccb9631c698acfdaa",
+    "url": "/Quote-App/static/media/bg3.cb3affd3.jpg"
+  },
+  {
+    "revision": "aea1e75edf149fe2ea5433e78a8b79f7",
+    "url": "/Quote-App/static/media/create.aea1e75e.svg"
+  },
+  {
+    "revision": "f375b15beb86bebe2f50c20f9502b016",
+    "url": "/Quote-App/static/media/read.f375b15b.svg"
+  }
+]);
